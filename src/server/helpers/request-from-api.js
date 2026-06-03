6a09f236a9558961f2050cec5024ec24bc8cc004
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2026-06-03 14:21:39 +0100

    sonar

--- src/server/helpers/request-from-api.js
+++ src/server/helpers/request-from-api.js
@@ -4,13 +4,11 @@ import { createApiHeadersForConfigBroker } from './broker-auth-helper.js'
 const GRANTS_CONFIG_BROKER_ENDPOINT = config.get('backend.apiEndpoint')
 
 export async function requestFromApi(endpoint, request) {
-  console.log(`Calling ${endpoint}`)
   if (!GRANTS_CONFIG_BROKER_ENDPOINT?.length) {
     return
   }
 
   const url = new URL(`/api/${endpoint}`, GRANTS_CONFIG_BROKER_ENDPOINT)
-  console.log(`Calling ${url.href}`)
   try {
     const response = await fetch(url.href, {
       method: 'GET',
