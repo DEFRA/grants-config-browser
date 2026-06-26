617693e62a378d95bceb22302aaa7b66fadca65a
aaroncarroll82 <20041831+aaroncarroll82@users.noreply.github.com>
2026-06-26 14:50:24 +0100

    Bit of debug on what comes back

--- src/server/auth/auth-callback.js
+++ src/server/auth/auth-callback.js
@@ -2,6 +2,11 @@ import { saveUserSession } from './save-user-session.js'
 import Boom from '@hapi/boom'
 
 const handler = async (request, h) => {
+  request.logger.info(`Auth callback payload: ${JSON.stringify(request.payload)}`)
+  request.logger.info(`Auth callback query: ${JSON.stringify(request.query)}`)
+  request.logger.info(`Auth callback headers: ${JSON.stringify(request.headers)}`)
+  request.logger.info(`Auth callback status: ${request.status}`)
+
   const credentials = await request.callback(h)
 
   if (!credentials) {
